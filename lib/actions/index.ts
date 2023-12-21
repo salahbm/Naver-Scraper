34335a20959899aeb3ccfff49026af807bee445a
salahbm <salahbm.001@gmail.com>
2023-12-21 16:20:16 +0900

    mongo db

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,15 @@
 "use server";
 
+import { connectDB } from "../database/mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
+    connectDB();
     const scrapeProduct = await scrapeAmazonProduct(productUrl);
+
     if (!scrapeProduct) return;
   } catch (error: any) {
     throw new Error(`Failed to create the Product: ${error.message}`);
