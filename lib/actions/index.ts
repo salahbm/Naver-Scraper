94845ec402218f0605b71e0fe548a046eaf714d1
salahbm <salahbm.001@gmail.com>
2023-12-21 17:43:22 +0900

    node mail

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,8 @@ import { connectDB } from "../database/mongoose";
 import Product from "../model/product.model";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -72,4 +74,47 @@ export async function getAllProducts() {
   }
 }
 
-export async function getSimilarProducts(element: any) {}
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if (!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addUserEmailToProduct(
+  productId: string,
+  userEmail: string
+) {
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) return;
+
+    const userExists = product.users.some(
+      (user: User) => user.email === userEmail
+    );
+
+    if (!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = await generateEmailBody(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
